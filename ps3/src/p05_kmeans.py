35848c70278f22cd1d6583a59b3d11d88cb28270
virusdoll <1029706584@qq.com>
2021-06-05 00:00:45 +0800

    finish: ps3

--- ps3/src/p05_kmeans.py
+++ ps3/src/p05_kmeans.py
@@ -1,169 +1,63 @@
-from __future__ import division, print_function
-import argparse
-import matplotlib.image as mpimg
+from matplotlib.image import imread
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-import random
 
+K = 16 # cluster numbers
+MAX_IT = 50 # max iterations
+CON_TH = 1 # convergence threshold
+img_path = '../data/peppers-large.tiff'
 
-def init_centroids(num_clusters, image):
-    """
-    Initialize a `num_clusters` x image_shape[-1] nparray to RGB
-    values of randomly chosen pixels of`image`
+def kmeans(k, x, max_it, con_th):
+    m, n = x.shape
 
-    Parameters
-    ----------
-    num_clusters : int
-        Number of centroids/clusters
-    image : nparray
-        (H, W, C) image represented as an nparray
+    # initial cluster centroids mu
+    # choose k x
+    np.random.seed(1)
+    mu = np.random.randint(255, size=(k, n))
+    loss = None
+    it = 0
 
-    Returns
-    -------
-    centroids_init : nparray
-        Randomly initialized centroids
-    """
+    # repeat until convergence
+    while True:
+        # update c
+        c = np.argmin(np.array([
+            np.linalg.norm(x - mu[j], axis=1)**2
+            for j in range(k)
+        ]), axis=0) # shape (m,)
+        
+        # update mu
+        for j in range(k):
+            x_j = x[np.where(c == j)]
+            if x_j.shape[0] > 0:
+                mu[j] = np.mean(x_j, axis=0)
+        
+        # if convergence
+        prev_loss = loss
+        loss = sum(np.linalg.norm(x - mu[c], axis=1)**2) / m
+        it += 1
+        print(it, loss)
+        if prev_loss == None:
+            continue
+        if np.abs(loss - prev_loss) < con_th or it == max_it:
+            break
 
-    # *** START YOUR CODE ***
-    # *** END YOUR CODE ***
+    # replace x with mu
+    x = mu[c]
 
-    return centroids_init
+    return x
 
-
-def update_centroids(centroids, image, max_iter=30, print_every=10):
-    """
-    Carry out k-means centroid update step `max_iter` times
-
-    Parameters
-    ----------
-    centroids : nparray
-        The centroids stored as an nparray
-    image : nparray
-        (H, W, C) image represented as an nparray
-    max_iter : int
-        Number of iterations to run
-    print_every : int
-        Frequency of status update
-
-    Returns
-    -------
-    new_centroids : nparray
-        Updated centroids
-    """
-
-    # *** START YOUR CODE ***
-    # raise NotImplementedError('update_centroids function not implemented')
-        # Usually expected to converge long before `max_iter` iterations
-                # Initialize `dist` vector to keep track of distance to every centroid
-                # Loop over all centroids and store distances in `dist`
-                # Find closest centroid and update `new_centroids`
-        # Update `new_centroids`
-    # *** END YOUR CODE ***
-
-    return new_centroids
-
-
-def update_image(image, centroids):
-    """
-    Update RGB values of pixels in `image` by finding
-    the closest among the `centroids`
-
-    Parameters
-    ----------
-    image : nparray
-        (H, W, C) image represented as an nparray
-    centroids : int
-        The centroids stored as an nparray
-
-    Returns
-    -------
-    image : nparray
-        Updated image
-    """
-
-    # *** START YOUR CODE ***
-    # raise NotImplementedError('update_image function not implemented')
-            # Initialize `dist` vector to keep track of distance to every centroid
-            # Loop over all centroids and store distances in `dist`
-            # Find closest centroid and update pixel value in `image`
-    # *** END YOUR CODE ***
-
-    return image
-
-
-def main(args):
-
-    # Setup
-    max_iter = args.max_iter
-    print_every = args.print_every
-    image_path_small = args.small_path
-    image_path_large = args.large_path
-    num_clusters = args.num_clusters
-    figure_idx = 0
-
-    # Load small image
-    image = np.copy(mpimg.imread(image_path_small))
-    print('[INFO] Loaded small image with shape: {}'.format(np.shape(image)))
-    plt.figure(figure_idx)
-    figure_idx += 1
-    plt.imshow(image)
-    plt.title('Original small image')
-    plt.axis('off')
-    savepath = os.path.join('.', 'orig_small.png')
-    plt.savefig(savepath, transparent=True, format='png', bbox_inches='tight')
-
-    # Initialize centroids
-    print('[INFO] Centroids initialized')
-    centroids_init = init_centroids(num_clusters, image)
-
-    # Update centroids
-    print(25 * '=')
-    print('Updating centroids ...')
-    print(25 * '=')
-    centroids = update_centroids(centroids_init, image, max_iter, print_every)
-
-    # Load large image
-    image = np.copy(mpimg.imread(image_path_large))
-    image.setflags(write=1)
-    print('[INFO] Loaded large image with shape: {}'.format(np.shape(image)))
-    plt.figure(figure_idx)
-    figure_idx += 1
-    plt.imshow(image)
-    plt.title('Original large image')
-    plt.axis('off')
-    savepath = os.path.join('.', 'orig_large.png')
-    plt.savefig(fname=savepath, transparent=True, format='png', bbox_inches='tight')
-
-    # Update large image with centroids calculated on small image
-    print(25 * '=')
-    print('Updating large image ...')
-    print(25 * '=')
-    image_clustered = update_image(image, centroids)
-
-    plt.figure(figure_idx)
-    figure_idx += 1
-    plt.imshow(image_clustered)
-    plt.title('Updated large image')
-    plt.axis('off')
-    savepath = os.path.join('.', 'updated_large.png')
-    plt.savefig(fname=savepath, transparent=True, format='png', bbox_inches='tight')
-
-    print('\nCOMPLETE')
-    plt.show()
-
+def compress(img):
+    m, n, d = img.shape
+    img_compress = kmeans(
+        K, img.reshape((m*n, d)), MAX_IT, CON_TH
+    ).reshape((m, n, d))
+    return img_compress
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--small_path', default='./peppers-small.tiff',
-                        help='Path to small image')
-    parser.add_argument('--large_path', default='./peppers-large.tiff',
-                        help='Path to large image')
-    parser.add_argument('--max_iter', type=int, default=150,
-                        help='Maximum number of iterations')
-    parser.add_argument('--num_clusters', type=int, default=16,
-                        help='Number of centroids/clusters')
-    parser.add_argument('--print_every', type=int, default=10,
-                        help='Iteration print frequency')
-    args = parser.parse_args()
-    main(args)
+    # read image
+    # shape (512, 512, 3)
+    img = imread(img_path)
+    img_compress = compress(img)
+    plt.imshow(img_compress)
+    plt.show()
+
